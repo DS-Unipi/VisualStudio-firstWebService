87338727718a82dab016263541b9aaf9937f43c9
George Pittas <sattip@hotmail.com>
2016-12-04 14:12:43 +0200

    add first webservice

---
+++ FirstWebService/IMathOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FirstWebService
+{
+	[DataContract]
+	public class MathsValues
+	{
+		[DataMember]
+		public int Number1 { get; set; }
+		[DataMember]
+		public int Number2 { get; set; }
+	}
+
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMathOperations" in both code and config file together.
+	[ServiceContract]
+	public interface IMathOperations
+	{
+		[OperationContract]
+		int Addition(MathsValues obj);
+		[OperationContract]
+		int Subtraction(MathsValues obj);
+		[OperationContract]
+		int Multiplication(MathsValues obj);
+		[OperationContract]
+		int Division(MathsValues obj);
+
+	}
+}
